d74241cc571927eb753bec889021b602c05a8f8c
Daniel Murphy <daniel.murphyx@gmail.com>
2024-12-23 19:54:10 +1100

    feat(main): Auth provider changes

--- client/src/hooks/AuthProvider.js
+++ client/src/hooks/AuthProvider.js
@@ -1,8 +1,44 @@
-import { useContext, createContext } from "react";
+import { useContext, createContext, useState } from "react";
+
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(localStorage.getItem("site") || "");
+  const loginAction = async (data) => {
+    try {
+      const response = await fetch("localhost:5000/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      const res = await response.json();
+      if (res.data) {
+        setUser(res.data.user);
+        setToken(res.token);
+        localStorage.setItem("site", res.token);
+        return;
+      }
+      throw new Error(res.message);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const logOut = () => {
+    setUser(null);
+    setToken("");
+    localStorage.removeItem("site");
+  };
+
+  return (
+    <AuthContext.Provider value={{ token, user, loginAction, logOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+
 };
 
 export default AuthProvider;
