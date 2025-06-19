e96060008651dcb88e71133209b78dd89067ca56
Daniel Murphy <daniel.murphyx@gmail.com>
2025-06-19 18:07:46 +1000

    fix(main): update py imports

--- spotipy_load/client..py
+++ spotipy_load/client..py
@@ -1,8 +1,15 @@
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyClientCredentials
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
 def get_spotify_client():
     return Spotify(auth_manager=SpotifyClientCredentials(
-        client_id='YOUR_CLIENT_ID',
-        client_secret='YOUR_CLIENT_SECRET'
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET
     ))
